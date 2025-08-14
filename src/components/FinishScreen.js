9cc89b6793f9938576c5104e47600e0e85226b03
Moamen Ashraf <moamenashraf533@gmail.com>
2025-08-14 16:13:44 +0300

    Finished React Quiz App

---
+++ src/components/FinishScreen.js
@@ -0,0 +1,27 @@
+function FinishScreen({ points, maxPossiblePoints, highscore, dispatch }) {
+  const precentage = (points / maxPossiblePoints) * 100;
+
+  let emoji;
+  if (precentage === 100) emoji = "🤘🏻";
+  if (precentage >= 80 && precentage < 100) emoji = "🥳";
+  if (precentage >= 50 && precentage < 80) emoji = "😁";
+  if (precentage >= 0 && precentage < 50) emoji = "🥲";
+  if (precentage === 0) emoji = "😭";
+  return (
+    <>
+      <p className="result">
+        <span>{emoji}</span>Your scored <strong>{points}</strong> out of{" "}
+        {maxPossiblePoints}({Math.ceil(precentage)}%)
+      </p>
+      <p className="highscore">Highscore: {highscore} points</p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "restart" })}
+      >
+        Restart Quiz
+      </button>
+    </>
+  );
+}
+
+export default FinishScreen;
